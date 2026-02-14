855a037bc19c6aef62349db6d25c25eb72d98add
xi2986041681@outlook.com <xi2986041681@outlook.com>
2026-02-14 18:43:58 +0800

    Site updated: 2026-02-14 18:43:47

---
+++ js/debounce.js
@@ -0,0 +1,25 @@
+// 防抖全局计时器
+let TT = null;    //time用来控制事件的触发
+// 防抖函数:fn->逻辑 time->防抖时间
+function debounce(fn, time) {
+    if (TT !== null) clearTimeout(TT);
+    TT = setTimeout(fn, time);
+}
+// 复制提醒
+document.addEventListener("copy", function () {
+    debounce(function () {
+        new Vue({
+            data: function () {
+                this.$notify({
+                    title: "复制成功！ (ง •_•)ง ",
+                    message: "转载记得备注原文链接哦 (*´ω`)人(´ω`*) ",
+                    position: 'top-left',
+                    offset: 50,
+                    showClose: true,
+                    type: "success",
+                    duration: 5000
+                });
+            }
+        })
+    }, 300);
+})
